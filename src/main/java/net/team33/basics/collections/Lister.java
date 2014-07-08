0da5a49f240f15288a3130e68ef5df528d460b80
Andi Kluge <andi.kluge@mobi4friends.de>
2014-07-08 14:44:06 +0200

    Finalized: Lister

--- src/main/java/net/team33/basics/collections/Lister.java
+++ src/main/java/net/team33/basics/collections/Lister.java
@@ -8,7 +8,7 @@ import java.util.List;
  *
  * @param <E> The element type of the {@link Collection} to be initialized.
  * @param <L> The type of the {@link List} to be initialized.
- * @param <R> The 'final' type of the {@link Lister} itself.
+ * @param <R> The finally intended type of the {@link Lister} itself.
  */
 @SuppressWarnings("StaticMethodOnlyUsedInOneClass")
 public class Lister<E, L extends List<E>, R extends Lister<E, L, R>> extends Collector<E, L, R> {
@@ -24,6 +24,9 @@ public class Lister<E, L extends List<E>, R extends Lister<E, L, R>> extends Col
         super(subject);
     }
 
+    /**
+     * Supplies a new instance to handle a specific {@link #subject}.
+     */
     public static <E, L extends List<E>> Lister<E, L, ?> apply(final L subject) {
         return new Simple<>(subject);
     }
@@ -31,13 +34,13 @@ public class Lister<E, L extends List<E>, R extends Lister<E, L, R>> extends Col
     /**
      * Substitutes {@link List#add(int, Object)} for the underlying {@link #subject}.
      *
-     * @return The related {@code Lister} itself in its 'final' representation. Of course not {@code null}.
-     * @throws UnsupportedOperationException (may occur only if used with an improper type of {@code subject})
-     *                                       if {@link List#add(int, Object)} is not supported by the underlying
-     *                                       {@link #subject}.
-     * @throws ClassCastException            (may occur only if used raw or forced in a mismatched class context)
-     *                                       if the class of the specified {@code element} prevents it from being added
-     *                                       to the underlying {@link #subject}.
+     * @return The related {@code Lister} itself in its finally intended representation. Of course not {@code null}.
+     * @throws UnsupportedOperationException if {@link List#add(int, Object)} is not supported by the underlying
+     *                                       {@link #subject}
+     *                                       (may occur only if used with an improper type of {@code subject}).
+     * @throws ClassCastException            if the class of the specified {@code element} prevents it from being added
+     *                                       to the underlying {@link #subject}
+     *                                       (may occur only if used raw or forced in a mismatched class context).
      * @throws NullPointerException          if the specified {@code element} is {@code null} and the underlying
      *                                       {@link #subject} does not permit {@code null} elements.
      * @throws IllegalArgumentException      if some property of the specified {@code element} prevents it from being
@@ -52,13 +55,13 @@ public class Lister<E, L extends List<E>, R extends Lister<E, L, R>> extends Col
     /**
      * Substitutes {@link List#addAll(int, Collection)} for the underlying {@link #subject}.
      *
-     * @return The related {@code Lister} itself in its 'final' representation. Of course not {@code null}.
-     * @throws UnsupportedOperationException (may occur only if used with an improper type of {@code subject})
-     *                                       if {@link List#addAll(int, Collection)} is not supported by the
-     *                                       underlying {@link #subject}.
-     * @throws ClassCastException            (may occur only if used raw or forced in a mismatched class context)
-     *                                       if the class of the specified {@code elements} prevents them from being
-     *                                       added to the {@code subject}.
+     * @return The related {@code Lister} itself in its finally intended representation. Of course not {@code null}.
+     * @throws UnsupportedOperationException if {@link List#addAll(int, Collection)} is not supported by the
+     *                                       underlying {@link #subject}
+     *                                       (may occur only if used with an improper type of {@code subject}).
+     * @throws ClassCastException            if the class of the specified {@code elements} prevents them from being
+     *                                       added to the {@code subject}
+     *                                       (may occur only if used raw or forced in a mismatched class context).
      * @throws NullPointerException          <ul>
      *                                       <li>if the {@link Collection} of {@code elements} is {@code null}</li>
      *                                       <li>if some of the specified {@code elements} are {@code null} and the
@@ -77,10 +80,10 @@ public class Lister<E, L extends List<E>, R extends Lister<E, L, R>> extends Col
     /**
      * Substitutes {@link List#remove(int)} for the underlying {@link #subject}.
      *
-     * @return The related {@code Lister} itself in its 'final' representation. Of course not {@code null}.
-     * @throws UnsupportedOperationException (may occur only if used with an improper type of {@code subject})
-     *                                       if {@link List#remove(int)} is not supported by the underlying
-     *                                       {@link #subject}.
+     * @return The related {@code Lister} itself in its finally intended representation. Of course not {@code null}.
+     * @throws UnsupportedOperationException if {@link List#remove(int)} is not supported by the underlying
+     *                                       {@link #subject}
+     *                                       (may occur only if used with an improper type of {@code subject}).
      * @throws IndexOutOfBoundsException     if the {@code index} is out of range: <tt>0 &lt;= index &lt; size()</tt>
      */
     public final R remove(final int index) {
@@ -91,13 +94,13 @@ public class Lister<E, L extends List<E>, R extends Lister<E, L, R>> extends Col
     /**
      * Substitutes {@link List#set(int, Object)} for the underlying {@link #subject}.
      *
-     * @return The related {@code Lister} itself in its 'final' representation. Of course not {@code null}.
-     * @throws UnsupportedOperationException (may occur only if used with an improper type of {@code subject})
-     *                                       if {@link List#set(int, Object)} is not supported by the underlying
-     *                                       {@link #subject}.
-     * @throws ClassCastException            (may occur only if used raw or forced in a mismatched class context)
-     *                                       if the class of the specified {@code element} prevents it from being added
-     *                                       to the underlying {@link #subject}.
+     * @return The related {@code Lister} itself in its finally intended representation. Of course not {@code null}.
+     * @throws UnsupportedOperationException if {@link List#set(int, Object)} is not supported by the underlying
+     *                                       {@link #subject}
+     *                                       (may occur only if used with an improper type of {@code subject}).
+     * @throws ClassCastException            if the class of the specified {@code element} prevents it from being added
+     *                                       to the underlying {@link #subject}
+     *                                       (may occur only if used raw or forced in a mismatched class context).
      * @throws NullPointerException          if the specified {@code element} is {@code null} and the underlying
      *                                       {@link #subject} does not permit {@code null} elements.
      * @throws IllegalArgumentException      if some property of the specified {@code element} prevents it from being
